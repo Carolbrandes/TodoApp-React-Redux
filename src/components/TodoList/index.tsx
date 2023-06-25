4c49c35d34c515d631ad7b6f04936be8ac03493a
carol <carolbrandes23@gmail.com>
2023-06-25 11:11:37 -0300

    todo filter by order

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -6,6 +6,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import moment from 'moment'
 
 
 const mapStateToProps = (state: any) => {
@@ -16,7 +17,8 @@ const mapStateToProps = (state: any) => {
 
 class TodoList extends React.Component {
     state = {
-        status: ''
+        status: '',
+        order: ''
     }
 
     handleStatus = (event: any) => {
@@ -25,13 +27,45 @@ class TodoList extends React.Component {
         })
     }
 
-    todosWithFilter = (todos: any) => {
-            return todos.filter((todo: any) => todo.status == this.state.status)
+    handleOrder = (event: any) => {
+        this.setState({
+            order: event.target.value
+        })
     }
 
+    todosWithFilter = (todos: any) => {
+        return todos.filter((todo: any) => todo.status == this.state.status)
+    }
+
+    todosWithOrder = (todos: any) => (
+        todos.sort((t1: any, t2: any) => {
+            const todo1 = moment(t1.date)
+            console.log("🚀 ~ file: index.tsx:43 ~ TodoList ~ todos.sort ~ todo1:", todo1)
+            const todo2 = moment(t2.date)
+            console.log("🚀 ~ file: index.tsx:45 ~ TodoList ~ todos.sort ~ todo2:", todo2)
+
+            if (this.state.order == 'firstnew') {
+                if (todo2 < todo1) return -1
+                if (todo1 > todo2) return 1
+                return 0
+            }
+
+            if (todo1 < todo2) {
+                return -1;
+            }
+            if (todo2 > todo1) {
+                return 1;
+            }
+
+            return 0;
+
+        })
+    )
+
     mytodos = (todos: any) => {
-       if(this.state.status) return this.todosWithFilter(todos) 
-       return todos
+        if (this.state.status) return this.todosWithFilter(todos)
+        if (this.state.order) return this.todosWithOrder(todos)
+        return todos
     }
 
     render() {
@@ -54,9 +88,27 @@ class TodoList extends React.Component {
                         </MenuItem>
                         <MenuItem value="p">Pendentes</MenuItem>
                         <MenuItem value="c">Concluídos</MenuItem>
-                       
+
                     </Select>
                 </FormControl>
+
+
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="order">Ordernar</InputLabel>
+                    <Select
+                        labelId="order"
+                        id="order"
+                        value={this.state.order}
+                        onChange={this.handleOrder}
+                        label="Ordernar"
+                    >
+                       
+                        <MenuItem value="firstnew">Mais recentes</MenuItem>
+                        <MenuItem value="firtsold">Mais antigos</MenuItem>
+
+                    </Select>
+                </FormControl>
+
 
                 {
                     todos?.length ? (
